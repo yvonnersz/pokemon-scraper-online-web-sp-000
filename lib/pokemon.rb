b2bc9c7b07a916716a741b126083de690f1a773e
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-05 00:19:44 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,7 +1,7 @@
 class Pokemon
   attr_accessor :id, :name, :type, :db
 
-  def self.initialize(id:n,name:,type:,db:)
+  def self.initialize(id:,name:,type:,db:)
     @id = id
     @name = name
     @type = type
